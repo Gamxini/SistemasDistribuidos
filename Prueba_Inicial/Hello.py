3774c78b0e130fb1f8f376cbb2526e273c522efa
Gamxini <gamchini@gmail.com>
2015-03-11 15:58:18 +0100

    Realizado primera Práctica

--- Prueba_Inicial/Hello.py
+++ Prueba_Inicial/Hello.py
@@ -1,3 +1,15 @@
-print "Hello world"
-print "no funciona"
-print  "fiestha"
+"""
+Version: 1.0
+Authors: Gambin Iñigo / Pastor Aznar
+Date: 11-03-15
+"""
+
+def drink(beer):
+ if beer == 0:
+  print ""
+ else:
+  print beer ," bottles of beer on the wall, ", beer ," bottles of beer."
+  print "Take one down, pass it around, ", beer-1 ," bottles of beer on the wall.\n"
+  drink(beer-1)
+
+drink(100)
